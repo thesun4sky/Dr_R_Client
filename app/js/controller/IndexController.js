b001574d803853531f335a48c0dce5fa084a4b0f
thesun4sky <thesun4sky@gmail.com>
2017-01-26 04:27:20 +0900

    수면,수유 그래프 기능 추가!

--- app/js/controller/IndexController.js
+++ app/js/controller/IndexController.js
@@ -6,10 +6,30 @@ var __IndexCtrl = function ($interval, $scope, $http, store, $state, $uibModal, 
     var userObject = store.get('obj');
     $scope.quantity = 4;
     $scope.selected_u_name ="";
+    $scope.sleep_series = ['수면', ''];
+    $scope.feed_series = ['수유', ''];
     $scope.loadingStyle = {'display': 'block'};
     $scope.showType = "list";
     $scope.a_name = userObject.a_name;
+    $scope.maxFeedRepeat = 5;
+    $scope.maxSleepRepeat = 5;
+    $scope.maxListRepeat = 10;
+    $scope.sleepArray = [];
+    $scope.sleepDates = [];
+    $scope.feedArray = [];
+    $scope.feedDates = [];
 
+    $scope.addMaxSleepRepeat = function() {
+        $scope.maxSleepRepeat += 5;
+    };
+
+    $scope.addMaxFeedRepeat = function() {
+        $scope.maxFeedRepeat += 5;
+    };
+
+    $scope.addMaxListRepeat = function() {
+        $scope.maxListRepeat += 5;
+    };
     $scope.dateTime = function(date) {
         date = date.toString();
         d = (date.split(' ')[0]);
@@ -48,6 +68,16 @@ var __IndexCtrl = function ($interval, $scope, $http, store, $state, $uibModal, 
             });
     };
 
+
+    $scope.colours = [{
+        fillColor: '#B2EBF4',
+        strokeColor: '#003399',
+        highlightFill: 'rgba(47, 132, 71, 0.8)',
+        highlightStroke: 'rgba(47, 132, 71, 0.8)',
+        backgroundColor: '#803690'
+    }];
+
+
     $scope.diaryListPost = function (u_id,u_name) {
         $scope.selected_u_name = u_name;
         $scope.loadingStyle = {'display': 'block'};
@@ -68,6 +98,10 @@ var __IndexCtrl = function ($interval, $scope, $http, store, $state, $uibModal, 
                     $scope.loadingStyle = {'display': 'none'};
                     $scope.sleepListPost(u_id,u_name);
                     $scope.feedListPost(u_id,u_name);
+
+                    $scope.maxFeedRepeat = 5;
+                    $scope.maxSleepRepeat = 5;
+                    $scope.maxListRepeat = 10;
                 }
                 else {
 
@@ -92,6 +126,27 @@ var __IndexCtrl = function ($interval, $scope, $http, store, $state, $uibModal, 
             .success(function (data, status, headers, config) {
                 if (data) { //존재하지 않음,아이디 사용가능
                     $scope.sleep_List = data;
+                    $scope.sleepArray = [];
+                    $scope.sleepDates = [];
+                    var prev_date = "";
+                    var total_sleep = 0;
+                    $scope.sleep_List.forEach(function (sleep_Data) {
+
+                        var s_date = new Date(sleep_Data.s_start);
+                        s_date = "" + (s_date.getYear()+1900) +"_"+ (s_date.getMonth()+1) +"_"+ s_date.getDate();
+
+                        if(prev_date == s_date || prev_date == "") {
+                            total_sleep += sleep_Data.s_total;
+                        }
+                        else{
+                            $scope.sleepArray.push(total_sleep);
+                            $scope.sleepDates.push(""+s_date);
+                            total_sleep = 0;
+                        }
+                        prev_date = s_date;
+                    });
+                    $scope.sleepArray = [$scope.sleepArray,[]];
+                       /* */
                     $scope.loadingStyle = {'display': 'none'};
                 }
                 else {
@@ -117,6 +172,26 @@ var __IndexCtrl = function ($interval, $scope, $http, store, $state, $uibModal, 
             .success(function (data, status, headers, config) {
                 if (data) { //존재하지 않음,아이디 사용가능
                     $scope.feed_List = data;
+                    $scope.feedArray = [];
+                    $scope.feedDates = [];
+                    var prev_date = "";
+                    var total_feed = 0;
+                    $scope.feed_List.forEach(function (feed_Data) {
+
+                        var f_date = new Date(feed_Data.f_start);
+                        f_date = "" + (f_date.getYear()+1900) +"_"+ (f_date.getMonth()+1) +"_"+ f_date.getDate();
+
+                        if(prev_date == f_date || prev_date == "") {
+                            total_feed += feed_Data.f_total;
+                        }
+                        else{
+                            $scope.feedArray.push(total_feed);
+                            $scope.feedDates.push(""+f_date);
+                            total_feed = 0;
+                        }
+                        prev_date = f_date;
+                    });
+                    $scope.feedArray = [$scope.feedArray,[]];
                     $scope.loadingStyle = {'display': 'none'};
                 }
                 else {
